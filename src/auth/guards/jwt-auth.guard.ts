0c4d26b6e7865942d55ef3b7e57129ceda656479
gerogelin <zhua0404@gmail.com>
2024-06-18 14:08:00 +0800

    feat: add comments.

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -2,5 +2,6 @@ import { Injectable } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
 
 
+// 檢查請求的 Authorization 標頭是否包含 jwt 令牌，並確保令牌格式正確（通常是 Bearer 令牌）, 驗證 jwt 的有效性, 提取用戶信息.
 @Injectable()
 export class JwtGuard extends AuthGuard('jwt') {}
